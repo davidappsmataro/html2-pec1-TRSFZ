f561654ad9912e6ca448cd12beab01fd61728007
David <david@grupcm.es>
2024-03-22 16:17:16 +0100

    antes de instalar Carrousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,62 @@
-
-console.log(`Hello ${name}`);
+/* console.log(`Hello ${name}`);
 try {
     
-   /*  if (process.env.NODE_ENV === 'development') {
-        console.log('Happy developing!');
-      } */
-      //console.log(process.env.NODE_ENV);
+  
       let {NODE_ENV, API_TOKEN} = process.env;
       console.log(NODE_ENV)
 } catch (error) {
     console.log(error)
-}
+} */
+
+const opcionesMenu = document.querySelector(".opciones_menu");
+const opcion = document.querySelectorAll("ul.opciones_menu > li > a");
+const bBurguer = document.querySelector("button.bburguer");
+const iBurguer = document.querySelector(".fa-bars");
+const iCloseBurguer = document.querySelector(".fa-circle-xmark");
+
+const toggleMenuBurguer = () => {
+  
+  iBurguer.classList.toggle("on");
+  iCloseBurguer.classList.toggle("on");
+  opcionesMenu.classList.toggle("on");
+
+  if (iBurguer.classList.contains("on")) {
+    iCloseBurguer.style.display = "none";
+    iBurguer.style.display = "block";
+    opcionesMenu.style.display = "none";
+  } else {
+    iCloseBurguer.style.display = "block";
+    opcionesMenu.style.display = "block";
+    iBurguer.style.display = "none";
+  }
+};
+
+const opcionSeleccionada = (e) => {
+  //Si hemos pulsado una de las opciones devolvemos 
+  //true para cerrar el menu burguer
+  let pulsadaOpcion = false;
+  opcion.forEach((opc) => {
+    if (opc.contains(e.target)) {
+      pulsadaOpcion = true;
+    }
+  });
+  return pulsadaOpcion;
+};
+
+bBurguer.addEventListener("click", toggleMenuBurguer);
+
+document.onclick = (e) => {
+  
+  if (
+    (!opcionesMenu.contains(e.target) && !bBurguer.contains(e.target)) ||
+    opcionSeleccionada(e) 
+  ) {
+    iBurguer.classList.add("on");
+    iCloseBurguer.classList.remove("on");
+    opcionesMenu.classList.remove("on");
+    iCloseBurguer.style.display = "none";
+    iBurguer.style.display = "block";
+    opcionesMenu.style.display = "none";
+  }
+  
+};
